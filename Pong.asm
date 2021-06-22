c2ad11e5fbd87b52fc4b07f5f8fdd3ee3ca11ea7
benTierneyA98 <80787207+benTierneyA98@users.noreply.github.com>
2021-06-22 16:37:53 +0100

    Add files via upload
    Write pixel to screen.

--- Pong.asm
+++ Pong.asm
@@ -10,10 +10,22 @@ CODE SEGMENT PARA 'CODE'
 
 	MAIN PROC FAR
 	
-		MOV DL,'A'
-		MOV AH, 6h
-		INT 21h
-	
+		MOV AH,00h 	;set configuration to video mode
+		MOV AL,13h 	;choose video mode
+		INT 10h		;execute configuration
+		
+		MOV AH,0Bh	;set configuration
+		MOV BH,00h	;to background colour
+		MOV BL,00h 	;choose black as background
+		INT 10h		;execute the configuration
+		
+		MOV AH,0Ch	;set configuration to writing a pixel
+		MOV AL,0Fh	;choose white as colour
+		MOV BH,00h	;set page number
+		MOV CX,0Ah	;set the column (X)
+		MOV DX,0Ah	;set the line (Y)
+		INT 10h		;execute the configuration
+		
 	
 		RET
 	MAIN ENDP
